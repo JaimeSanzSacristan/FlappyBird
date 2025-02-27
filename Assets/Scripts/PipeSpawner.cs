6af087c224f5b7e3efdff3142bfd66af2e9d4d5d
Jaime <jaimesanz192@gmail.com>
2025-02-27 19:11:44 +0100

    GameObjectPool añadido

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -1,23 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-    public GameObject pipePrefab;
+    public GameObjectPool pipePool;  // Referencia al pool
 
     public float heightRange = 0.5f;
-
     public float maxTime = 1.75f;
 
     private float timer;
+
     void Start()
     {
         SpawnPipe();
     }
 
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
@@ -32,9 +29,28 @@ public class PipeSpawner : MonoBehaviour
     public void SpawnPipe()
     {
         Vector3 spawnPosition = transform.position + new Vector3(0, Random.Range(-heightRange, heightRange));
-        GameObject newPipe;
-        newPipe = Instantiate(pipePrefab, spawnPosition, Quaternion.identity);
-        Destroy(newPipe, 15f);
+
+        GameObject newPipe = pipePool.GetInactiveGameObject(); // Saca una tubería del pool
+
+        if (newPipe != null)
+        {
+            newPipe.transform.position = spawnPosition;
+            newPipe.transform.rotation = Quaternion.identity;
+            newPipe.SetActive(true);
+
+            // Desactiva la tubería después de 15 segundos para devolverla al pool
+            StartCoroutine(ReturnPipeAfterTime(newPipe, 15f));
+        }
+        else
+        {
+            Debug.LogWarning("No hay tuberías disponibles en el pool y no se puede expandir.");
+        }
     }
 
+    private IEnumerator ReturnPipeAfterTime(GameObject pipe, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        pipe.SetActive(false); // En vez de destruirla, se desactiva para que el pool la reutilice
+    }
 }
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Versión de Unity utilizada: 2022.3.56f1
